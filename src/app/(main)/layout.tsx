c22642331299a8c1526e4675bc18050a87b10e3d
treaddevs <s.treadwell11@gmail.com>
2025-02-03 20:58:58 -0500

    Adding with Lucia Auth complete

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import { validateRequest } from "@/auth";
+import { redirect } from "next/navigation";
+import SessionProvider from "./SessionProvider";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await validateRequest();
+
+  if (!session.user) redirect("/login");
+
+  return <SessionProvider value={session}>{children}</SessionProvider>;
+}
