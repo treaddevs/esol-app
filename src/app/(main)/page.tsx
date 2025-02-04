9246539fc78c63741f53dd34b2c2b942ac17b96c
treaddevs <s.treadwell11@gmail.com>
2025-02-03 22:43:00 -0500

    Adding sidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>Front page</main>
+    <main className="h-[200vh] w-full bg-red-50">
+      <div className="w-full">Front page</div>
+    </main>
   );
 }
