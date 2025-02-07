299a73382d088fe88080b99cf3612ea1c66ad031
treaddevs <s.treadwell11@gmail.com>
2025-02-06 23:21:55 -0500

    Adding for you + following feeds

--- src/app/api/posts/following/route.ts
+++ src/app/api/posts/following/route.ts
@@ -16,6 +16,15 @@ export async function GET(req: NextRequest) {
     }
 
     const posts = await prisma.post.findMany({
+      where: {
+        user: {
+          followers: {
+            some: {
+              followerId: user.id,
+            },
+          },
+        },
+      },
       include: getPostDataInclude(user.id),
       orderBy: { createdAt: "desc" },
       take: pageSize + 1,
