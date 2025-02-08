6da980fbf6c51893e1b4b3d32b6eb90a449b70f8
treaddevs <s.treadwell11@gmail.com>
2025-02-08 15:44:05 -0500

    Updating user profile and avatar

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -23,3 +23,10 @@ export type LoginValues = z.infer<typeof loginSchema>;
 export const createPostSchema = z.object({
   content: requiredString,
 });
+
+export const updateUserProfileSchema = z.object({
+  displayName: requiredString,
+  bio: z.string().max(1000, "Bio must not exceed 1,000 characters"),
+});
+
+export type UpdateUserProfileValues = z.infer<typeof updateUserProfileSchema>;
